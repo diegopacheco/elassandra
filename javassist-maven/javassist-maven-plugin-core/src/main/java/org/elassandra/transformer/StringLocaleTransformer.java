cb026eab781ca4a2ea3b89049ffabaddf7461387
Vincent Royer <vroyer@vroyer.org>
2016-12-04 20:49:05 +0100

    fix javasist

--- javassist-maven/javassist-maven-plugin-core/src/main/java/org/elassandra/transformer/StringLocaleTransformer.java
+++ javassist-maven/javassist-maven-plugin-core/src/main/java/org/elassandra/transformer/StringLocaleTransformer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.strapdata.transformer;
+package org.elassandra.transformer;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
